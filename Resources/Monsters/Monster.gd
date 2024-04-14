5c3e9b564e31ea87911a0cf498efdd3e9c29ef96
btCharlie <thomas.b.charlie@gmail.com>
2024-04-14 23:28:03 +0200

    Test monster setup
    test monster has some graphics

--- Resources/Monsters/Monster.gd
+++ Resources/Monsters/Monster.gd
@@ -5,10 +5,17 @@ enum MONSTER_TYPE {FIRE, WATER, GRASS}
 
 @export var name:String
 @export var type:Array[ElementalType]
+
+@export var ally_graphic:Texture2D
+@export var foe_graphic:Texture2D
+@export var inventory_graphic:Texture2D
+@export var map_graphic:Texture2D
+
 @export_range(1, 100) var attack:int
 @export_range(1, 100) var spec_attack:int
 @export_range(1, 100) var defense:int
 @export_range(1, 100) var spec_defense:int
+@export_range(1, 100) var speed:int
 @export_range(10, 1000) var max_health:int
 @export_range(10, 1000) var max_stress:int
 
